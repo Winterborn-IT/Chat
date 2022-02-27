3f0740dd09005537d3b0f1382513ecd8a7306668
Ilnur Baltaev <61975462+Winterborn-IT@users.noreply.github.com>
2022-02-27 18:58:22 +0400

    First commit

---
+++ Command/src/main/java/constants/Command.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Command {
+    public static final String END = "/end";
+    public static final String AUTH = "/auth";
+    public static final String AUTH_OK = "/auth_ok";
+}
--- client/src/main/java/level2/lesson4/Controller.java
+++ client/src/main/java/level2/lesson4/Controller.java
@@ -3,12 +3,18 @@ package level2.lesson4;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -30,6 +36,8 @@ public class Controller implements Initializable {
     public HBox authPanel;
     @FXML
     public HBox msgPanel;
+    @FXML
+    public ListView<String> clientList;
 
     private Socket socket;
     private static final int PORT = 8189;
@@ -41,6 +49,9 @@ public class Controller implements Initializable {
     private boolean authenticated;
     private String nickname;
     private Stage stage;
+    private Stage regStage;
+    private RegController regController;
+
 
     public void setAuthenticated(boolean authenticated) {
         this.authenticated = authenticated;
@@ -48,6 +59,9 @@ public class Controller implements Initializable {
         authPanel.setManaged(!authenticated);
         msgPanel.setVisible(authenticated);
         msgPanel.setManaged(authenticated);
+        clientList.setVisible(authenticated);
+        clientList.setManaged(authenticated);
+
 
         if (!authenticated) {
             nickname = "";
@@ -97,6 +111,9 @@ public class Controller implements Initializable {
                                 setAuthenticated(true);
                                 break;
                             }
+                            if (str.equals("/reg_ok") || str.equals("/reg_no")) {
+                                regController.result(str);
+                            }
                         } else {
                             textArea.appendText(str + "\n");
                         }
@@ -104,12 +121,23 @@ public class Controller implements Initializable {
                     //цикл работы
                     while (authenticated) {
                         String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                break;
+                            }
+                            if (str.startsWith("/clientlist")) {
+                                String[] token = str.split(" ");
 
-                        if (str.equals("/end")) {
-                            break;
+                                Platform.runLater(() -> {
+                                    clientList.getItems().clear();
+                                    for (int i = 1; i < token.length; i++) {
+                                        clientList.getItems().add(token[i]);
+                                    }
+                                });
+                            }
+                        } else {
+                            textArea.appendText(str + "\n");
                         }
-
-                        textArea.appendText(str + "\n");
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -166,5 +194,53 @@ public class Controller implements Initializable {
         Platform.runLater(() -> {
             stage.setTitle(title);
         });
+    }
+
+    public void clientlistMouseAction(MouseEvent mouseEvent) {
+        String receiver = clientList.getSelectionModel().getSelectedItem();
+        textField.setText(String.format("/w %s ", receiver));
+    }
+
+    private void createRegStage() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("reg.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+
+            regStage = new Stage();
+
+            regStage.setTitle("HelloTalk! registration");
+            regStage.setScene(scene);
+
+            regController = fxmlLoader.getController();
+            regController.setController(this);
+
+            regStage.initStyle(StageStyle.UTILITY);
+            regStage.initModality(Modality.APPLICATION_MODAL);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void tryToReg(ActionEvent actionEvent) {
+        if (regStage == null) {
+            createRegStage();
+        }
+        regStage.show();
+    }
+
+    public void registration(String login, String password, String nickname) {
+        String msg = String.format("/reg %s %s %s", login, password, nickname);
+
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
